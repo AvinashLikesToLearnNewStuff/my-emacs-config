7c4687fe898984999d87f2ffda541c85e893a0de
AvinashProjects <106229044+AvinashLikesToLearnNewStuff@users.noreply.github.com>
2023-05-13 13:42:54 +0530

    added reloading control c , r

--- config.el
+++ config.el
@@ -26,3 +26,20 @@
   ("M-x" . smex))
 
 (global-set-key (kbd "C-x b") 'ibuffer)
+
+(use-package avy
+:ensure t
+:bind
+("M-s" . avy-goto-char))
+
+(defun config-visit ()
+  (interactive)
+  (find-file "~/.emacs.d/config.org"))
+
+(global-set-key (kbd "C-c e") 'config-visit)
+
+(defun config-reload ()
+  (interactive)
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+
+(global-set-key (kbd "C-c r") 'config-reload)
