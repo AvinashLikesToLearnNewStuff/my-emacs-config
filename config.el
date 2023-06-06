a001115e9e8eb6005a87de1365495db14e3310f5
AvinashProjects <106229044+AvinashLikesToLearnNewStuff@users.noreply.github.com>
2023-06-06 12:13:48 +0530

    installed keychords and set jk to come out of insert mode

--- config.el
+++ config.el
@@ -545,5 +545,5 @@
 
 ;;Exit insert mode by pressing j and then j quickly
 (setq key-chord-two-keys-delay 0.5)
-(key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+(key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
 (key-chord-mode 1)
